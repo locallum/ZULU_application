78915f86f0a23e4610157713e69d58093eaf23aa
Esther Wu <z5416636@ad.unsw.edu.au>
2025-04-09 16:35:57 +1000

    Replace template code with a blank dashboard page

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+/**
+ * The dashboard screen displays all main website content.
+ */
+const Dashboard = () => {
+  return (
+    <>
+      <div>
+        Dashboard screen
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react';
+// import Button from "@mui/material/Button";
+
+/**
+ * The login screen displays a login form to authenticate an existing user.
+ */
+const Login = () => {
+  return (
+    <>
+      <div>
+        Login screen
+      </div>
+    </>
+  );
+};
+
+export default Login;
