3db62ba1a48b5c297dd62e070481fc1c482f9840
Esther Wu <z5416636@ad.unsw.edu.au>
2025-04-10 18:10:04 +1000

    Restyle header and nav bar

--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -1,24 +1,35 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
+
+// Custom styling for nav text buttons
+const TextButton = styled(Button)({
+  color: 'white',
+  '&:hover': {
+    backgroundColor: '#1976d2',
+  },
+});
 
 /**
  * The header component contains a navigation bar with a login button.
  */
 const Header = () => {
-  const navigate = useNavigate();
-
   return (
-    <div className="header-container">
+    <header className="header-container">
         <h2>ZULU Urban Metrics</h2>
 
-        <Button variant="text" onClick={() => navigate('/')}>
-          Dashboard
-        </Button>
-
-        <Button variant="contained" onClick={() => navigate('/login')}>
-          Login
-        </Button>
-    </div>
+        <nav className="nav-container">
+          <TextButton variant="text" component={Link} to="/">
+            Dashboard
+          </TextButton>
+          <TextButton variant="text" href="https://app.swaggerhub.com/apis/zulu-89f/Zulu_transport_API/1.0.0">
+            Swagger API
+          </TextButton>
+          <Button variant="contained" component={Link} to="/">
+            Login
+          </Button>
+        </nav>
+    </header>
   );
 };
 
