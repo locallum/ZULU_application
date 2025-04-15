0820771514eae317e70c9ea90d70579d9e7d6b87
Callum <105720890+locallum@users.noreply.github.com>
2025-04-15 15:25:15 +1000

    got backend serer.js working with EB deployment

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,15 @@
 const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-
+const path = require('path');
 const app = express();
-app.use(cors());
 
-const port = process.env.PORT || 3000;
-const retrievalURL = 'https://slzykzeusf.execute-api.us-east-1.amazonaws.com/prod/population/v1';
-const visualisationURL = 'https://f8jc59emd2.execute-api.us-east-1.amazonaws.com/dev/population/visualisation/v1';
+// Serve static files from the "frontend/dist" directory
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 
-// Route for handling the retrieval API request
-app.get('/retrieve/singleSuburb', async (req, res) => {
-    const { suburb, startYear, endYear } = req.query;
-    try {
-        const response = await axios.get(
-            retrievalURL,
-            {
-                params: { suburb, startYear, endYear }
-            }
-        );
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).send('Error fetching data from retrieval API');
-    }
+// Fallback to index.html for routing in React app
+app.get('/{*all}', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running...");
 });
