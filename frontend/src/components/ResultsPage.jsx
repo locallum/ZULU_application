f84547d4d1c0e231b974fa7669018e54ba4ee575
Callum <105720890+locallum@users.noreply.github.com>
2025-04-18 17:44:36 +1000

    added donwload button

--- frontend/src/components/ResultsPage.jsx
+++ frontend/src/components/ResultsPage.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import DownloadIcon from "@mui/icons-material/Download";
 
 const styles = {
   page: {
@@ -7,9 +10,15 @@ const styles = {
     paddingLeft: "60px",
     paddingRight: "60px",
   },
+  download: {
+    position: "absolute",
+    right: "60px",
+  },
 };
 
 const ResultsPage = ({ selected, resultsImg }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const formatSuburbs = (suburbs) => {
     if (suburbs.length === 0) return "";
     if (suburbs.length === 1) return suburbs[0];
@@ -17,27 +26,50 @@ const ResultsPage = ({ selected, resultsImg }) => {
     return `${suburbs.slice(0, -1).join(", ")} & ${suburbs[suburbs.length - 1]}`;
   };
 
+  const handleDownload = () => {
+    setIsLoading(true);
+
+    // Simulate a short delay to show loading state
+    setTimeout(() => {
+      const link = document.createElement("a");
+      link.href = `data:image/png;base64,${resultsImg}`;
+      link.download = `${selected.join("-")}.png`;
+      link.click();
+      setIsLoading(false);
+    }, 1000); // adjust delay if necessary
+  };
+
   if (resultsImg === "") return null;
 
   return (
     <div style={styles.page}>
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<DownloadIcon />}
+        sx={styles.download}
+        onClick={handleDownload}
+        loading={isLoading}
+      >
+        Download
+      </Button>
+
       <Typography
         variant="h4"
         sx={{ fontWeight: "bold", color: "var(--accent)" }}
       >
         {formatSuburbs(selected)}
       </Typography>
-      <Typography
-        variant="h4"
-        sx={{ fontWeight: "bold" }}
-      >
+
+      <Typography variant="h4" sx={{ fontWeight: "bold" }}>
         Population Projection{selected.length > 1 ? "s" : ""}
       </Typography>
+
       {resultsImg && (
         <img
           src={`data:image/png;base64,${resultsImg}`}
           alt="Generated Graph"
-          style={{ maxWidth: "100%", marginTop: "40px", borderRadius: "10px" }}
+          style={{ maxWidth: "40%", marginTop: "40px", borderRadius: "10px" }}
         />
       )}
     </div>
