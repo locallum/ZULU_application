91352595996cc88997f8b214822a8daeafce11e7
esther-xx-wu <estherxxwu@gmail.com>
2025-04-29 10:11:24 +1000

    Change lint error to warn

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -4,7 +4,8 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ["**/*.{js}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { rules: {"no-unused-vars": "warn"} }
 ]);
